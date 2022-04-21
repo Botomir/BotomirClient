bc43394f921f2b5a34a8dc4d658254ff4969feb6
SoorajModi <soomodi@gmail.com>
2022-04-21 17:21:09 -0400

    Add content for home page

--- src/pages/home/index.js
+++ src/pages/home/index.js
@@ -2,8 +2,13 @@ import React from 'react';
 
 function Home() {
   return (
-    <div>
-      Hello world!
+    <div className="flex flex-col justify-center items-center h-96 space-y-5">
+      <span className="text-5xl">Botomir</span>
+      <span className="text-xl">Your friendly neighbourhood Discord bot</span>
+      <div className="flex flex-col space-y-2">
+        <a href="https://discord.com/api/oauth2/authorize?client_id=782053704227487754%20%20%20%20%20%20%20%20&permissions=469887088&scope=bot" target="_blank" rel="noreferrer" className="btn btn-primary w-96">Add to Server</a>
+        <a href="https://paypal.me/soorajmodi" target="_blank" rel="noreferrer" className="btn btn-primary w-96">Donate</a>
+      </div>
     </div>
   );
 }
