b1fcd606e2561f91dc3f91ebba47cfba5b6cf8c8
SoorajModi <soomodi@gmail.com>
2022-04-21 01:49:14 -0400

    Add subpages

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,7 +1,19 @@
 import Home from './home';
 import NotFound from './notFound';
+import About from './about';
+import Contact from './contact';
+import Docs from './docs';
+import Stats from './stats';
+import Login from './login';
+import SignUp from './signUp';
 
 export {
   Home,
   NotFound,
+  About,
+  Contact,
+  Docs,
+  Stats,
+  Login,
+  SignUp,
 };
