5fc9f114ccc02e79adb3972ff6e88d11cffca311
Sooraj <soomodi@gmail.com>
2022-04-21 02:05:40 -0400

    Subpages (#10)
    * Update tab icon
    
    * Add subpages
    
    * Add github icon to footer

--- src/components/navbar/index.js
+++ src/components/navbar/index.js
@@ -6,13 +6,12 @@ function Navbar() {
       <div className="flex-1">
         <a className="btn btn-link normal-case text-xl text-base-content hover:no-underline" href="/">Botomir</a>
         <ul className="menu menu-horizontal p-0">
-          <li><a className="btn btn-link normal-case text-base-content" href="/">Docs</a></li>
-          <li><a className="btn btn-link normal-case text-base-content" href="/">Stats</a></li>
+          <li><a className="btn btn-link normal-case text-base-content" href="/docs">Docs</a></li>
         </ul>
       </div>
       <div className="flex-none pr-3 space-x-3">
-        <a href="/" className="btn btn-outline">Login</a>
-        <a href="/" className="btn">Sign Up</a>
+        <a href="/login" className="btn btn-outline">Login</a>
+        <a href="/signup" className="btn">Sign Up</a>
       </div>
     </div>
   );
--- src/router/index.js
+++ src/router/index.js
@@ -1,12 +1,20 @@
 import React from 'react';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import { Home, NotFound } from '../pages';
+import {
+  About, Contact, Docs, Home, Login, NotFound, SignUp, Stats,
+} from '../pages';
 
 function Router() {
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<Home />} />
+        <Route path="/docs" element={<Docs />} />
+        <Route path="/stats" element={<Stats />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/contact" element={<Contact />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/signup" element={<SignUp />} />
         <Route path="*" element={<NotFound />} />
       </Routes>
     </BrowserRouter>
