c89ee30db68069166165cba4c3016c74fd656ee9
SoorajModi <soomodi@gmail.com>
2022-04-22 00:18:28 -0400

    Fix add to server link + refactor

--- src/pages/home/index.js
+++ src/pages/home/index.js
@@ -6,8 +6,22 @@ function Home() {
       <span className="text-5xl">Botomir</span>
       <span className="text-xl">Your friendly neighbourhood Discord bot</span>
       <div className="flex flex-col space-y-2">
-        <a href="https://discord.com/api/oauth2/authorize?client_id=782053704227487754%20%20%20%20%20%20%20%20&permissions=469887088&scope=bot" target="_blank" rel="noreferrer" className="btn btn-primary w-96 normal-case">Add to Server</a>
-        <a href="https://paypal.me/soorajmodi" target="_blank" rel="noreferrer" className="btn btn-primary w-96 normal-case">Donate</a>
+        <a
+          href="https://discord.com/api/oauth2/authorize?client_id=781298262586032129&permissions=469887088&scope=bot"
+          target="_blank"
+          rel="noreferrer"
+          className="btn btn-primary w-96 normal-case"
+        >
+          Add to Server
+        </a>
+        <a
+          href="https://paypal.me/soorajmodi"
+          target="_blank"
+          rel="noreferrer"
+          className="btn btn-primary w-96 normal-case"
+        >
+          Donate
+        </a>
       </div>
     </div>
   );
